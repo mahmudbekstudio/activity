1141d7de4bf88d7026dc5f6fd8dff4afb51f50bb
Makhmud <mahmudbekstudio@gmail.com>
2018-10-01 11:01:52 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script src="./App.js"></script>
+<style lang="scss" src="./App.scss"></style>
+
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
